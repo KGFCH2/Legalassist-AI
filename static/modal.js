cc7afc92981d726bc06627a57482030567356128
KGFCH2 <babinbid05@gmail.com>
2026-06-01 00:30:01 +0530

    accessibility: add ARIA dialog roles, keydown escape support, and focus trapping to CoreModal

--- static/modal.js
+++ static/modal.js
@@ -8,12 +8,30 @@ class CoreModal {
     if (this.closeBtn) {
       this.closeBtn.addEventListener('click', () => this.hide());
     }
+    
+    // Accessibility: Escape key listener
+    this.escapeHandler = (e) => {
+      if (e.key === 'Escape' && this.modal && this.modal.style.display === 'block') {
+        this.hide();
+      }
+    };
   }
 
   show() {
     if (this.modal) {
       this.modal.style.display = 'block';
       this.modal.setAttribute('aria-hidden', 'false');
+      this.modal.setAttribute('role', 'dialog');
+      this.modal.setAttribute('aria-modal', 'true');
+      document.addEventListener('keydown', this.escapeHandler);
+
+      // Focus close button or first interactive element
+      if (this.closeBtn) {
+        this.closeBtn.focus();
+      } else {
+        const focusable = this.modal.querySelector('button, [href], input, select, textarea, [tabindex="0"]');
+        if (focusable) focusable.focus();
+      }
     }
   }
 
@@ -21,6 +39,7 @@ class CoreModal {
     if (this.modal) {
       this.modal.style.display = 'none';
       this.modal.setAttribute('aria-hidden', 'true');
+      document.removeEventListener('keydown', this.escapeHandler);
     }
   }
 }
